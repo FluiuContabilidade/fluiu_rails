bfbfde0c1e6e6e7b5d6f1abd57a4d794a0dd0303
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-03-27 16:46:08 -0300

    added confirmable to user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: "registrations" }
+
   devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
+    get 'user/new', to: 'users#new'
+    post 'user/create', to: 'users/registrations#custom_account_creation', as: :create_user
   end
 
+  root to: 'pages#home_page'
   get 'home', to: 'pages#home_page'
-  
+
+
+  ## USERS
+
+
 end

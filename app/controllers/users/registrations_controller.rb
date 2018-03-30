d71b1bc0cde3bac04eee7327c248dcbc3d8ac8ec
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-03-30 10:35:23 -0300

    devise email setup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -74,8 +74,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
-    
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:password, :password_confirmation, :email, :telephone, :cpf, :company, :cnpj)
     end

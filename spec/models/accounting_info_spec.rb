cefd6eb30943995f7a4b6304acfeacaca835e509
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-03 16:32:11 -0300

    fixing tests

--- spec/models/accounting_info_spec.rb
+++ spec/models/accounting_info_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe AccountingInfo, type: :model do
+RSpec.describe AccountingInfos, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end

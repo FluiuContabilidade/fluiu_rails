e8e02d01adacefb9658e291f7eade796faa7c592
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-04 15:13:18 -0300

    added styling for missing invoices notification

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,7 @@
 module PagesHelper
 
   def self.mount_missing_invoice_message user
-    if user.has_month_invoices? Time.now.strftime('%Y-m')
+    if user.has_month_invoices? Time.now.strftime('%Y-%m')
       return nil
     else
       return "Você não enviou as notas fiscais deste mês. Clique em Rotinas Contábeis para enviá-las."

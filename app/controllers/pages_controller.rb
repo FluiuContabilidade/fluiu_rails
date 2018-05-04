1501993cf4d26defd06b3c19f3f697e2952a679d
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2018-05-04 16:02:40 -0300

    added tax percentage extimative to main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,7 @@ class PagesController < ApplicationController
     @total_in = current_user.get_monthly_invoice_total(Time.now.strftime('%Y-%m'), 'in')
     @total_out = current_user.get_monthly_invoice_total(Time.now.strftime('%Y-%m'), 'out')
     @message = PagesHelper.mount_missing_invoice_message current_user
+    @tax_extimative = current_user.get_company_tax_percentage
   end
 
 end
